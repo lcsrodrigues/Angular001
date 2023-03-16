819c4fa72ffd5daa5249926aadf04065d82e9885
lcsrodrigues <lcs-rodrigues@hotmail.com>
2023-03-16 20:30:50 +0000

    adicionado regra para desabilitar botão quando chegar no limite do array de frases

---
+++ app1/src/app/panel/frases-mock.ts
@@ -0,0 +1,8 @@
+import Frases from 'app/shared/frases.model';
+
+export const FRASES: Frases[] = [
+  { fraseEng: 'Phrase 01', frasePtBr: 'Frase 01' },
+  { fraseEng: 'Phrase 02', frasePtBr: 'Frase 02' },
+  { fraseEng: 'Phrase 03', frasePtBr: 'Frase 03' },
+  { fraseEng: 'Phrase 04', frasePtBr: 'Frase 04' }
+];
--- app1/src/app/panel/panel.component.ts
+++ app1/src/app/panel/panel.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import Frases from 'app/shared/frases.model';
+import { FRASES } from './frases-mock';
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent implements OnInit {
-  constructor() {}
+  public frases: Frases[] = FRASES;
+  public instrucao: string = 'Traduza a frase:';
+  public resposta: string;
+  public rodada: number = 0;
+  public rodadaFrase: Frases;
+  public isDisabled: boolean = false;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+  }
   ngOnInit() {}
+
+  public atualizaResposta(response: Event): void {
+    this.resposta = (<HTMLInputElement>response.target).value;
+  }
+
+  public verificarResposta(): void {
+    if (this.resposta === this.frases[this.rodada].frasePtBr) {
+      console.log('Acertou');
+    } else {
+      console.log('Errou');
+    }
+    if (this.frases.length <= this.rodada + 1) {
+      this.isDisabled = true;
+    } else {
+      this.rodadaFrase = this.frases[this.rodada];
+    }
+
+    this.rodada++;
+  }
 }
